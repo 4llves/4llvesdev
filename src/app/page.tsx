8d1ee59becf5147540fba0dcff4ba745da0d95cb
4llves <jhonatab4@gmail.com>
2024-03-10 23:00:23 -0300

    feat: edit home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <div className="h-6 w-6 bg-slate-50 mx-auto"></div>
+  return (
+    <div className="flex items-center justify-center h-[360px] w-[720px] mx-auto">
+      <section>
+        <h1 className="text-5xl">Alves Jhonata</h1>
+        <strong>Desenvolvedor RPA | Front-end</strong>
+        <p>Desvendando problemas e criando soluções através da tecnologia</p>
+      </section>
+      <section>
+        <img src="" alt="" />
+      </section>
+    </div>
+  )
 }
