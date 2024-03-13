2d0d02d860861a541fc467b42d1687b6c992fdb6
4llves <jhonatab4@gmail.com>
2024-03-12 23:55:57 -0300

    feat: add about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,10 +5,26 @@ import avatar from '../../assets/avatar.json'
 
 export default function About() {
   return (
-    <div className="w-full bg-slate-500">
-      <h1 className="text-green-400">about</h1>
+    <div className="flex gap-10 w-3/6">
+      <section className="flex">
+        <div className="items-center justify-center gap-4">
+          <h1 className="text-left text-5xl font-bold text-slate-300">
+            Sobre mim
+          </h1>
+          <Lordicon icon={avatar} />
+        </div>
+      </section>
 
-      <Lordicon icon={avatar} />
+      <section className="flex">
+        <p>
+          <strong>Faaala galerinhaaaaaa...</strong> Tudo bem por ai? 😁 <br />
+          Sou <strong>Jhonata Alves</strong> um jovem do interior do Pará que se
+          interessou por tecnologia bem novo. Comecei na área como Tecnico em
+          Manutenção de Computadores em 2010. Agora em 2024, sou criador de
+          robôs. Tenho 26 anos, nasci, cresci e moro em Goianésia do Pará - PA,
+          Brasil. Gosto de tocar violão, cajon, e ler mangás.
+        </p>
+      </section>
     </div>
   )
 }
