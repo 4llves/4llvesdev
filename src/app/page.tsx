1d7b33aee3efe7650d167d7aaf592f455fe9c446
4llves <jhonatab4@gmail.com>
2024-03-14 13:33:49 -0300

    feat: style h1 home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,21 @@ const quantico = Quantico({
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center gap-10">
+    <div className="flex items-center justify-center gap-10 px-10">
       <section className="flex flex-col gap-y-6">
         <h1
-          className={`${quantico.className} font-sans text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-red-500 to-red-500/30`}
+          className={`${quantico.className} text-6xl md:text-7xl font-extrabold bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient`}
         >
           Alves Jhonata
         </h1>
         <div>
-          <strong>Desenvolvedor RPA | Front-end</strong>
-          <p>Desvendando problemas e criando soluções através da tecnologia</p>
+          <strong className="text-xl">Desenvolvedor RPA | Front-end</strong>
+          <p className="text-lg">
+            Desvendando problemas e criando soluções através da tecnologia
+          </p>
         </div>
 
-        <p className={`${quantico.className} font-bold`}>
+        <p className={`${quantico.className} text- font-bold`}>
           Pressione{' '}
           <button className="bg-slate-100 text-blue-700 px-1 rounded-sm">
             Ctrl + K
@@ -28,7 +30,7 @@ export default function Home() {
           para Iniciar
         </p>
       </section>
-      <section>
+      <section className="">
         <Image
           className="shadow-person rounded-lg"
           src="https://github.com/4llves.png"
